4a393b8fb83a9fb4f86876ad3e7f193db9e132e8
Aurelio <aurelio74123@gmail.com>
2023-01-04 10:47:38 -0300

    login social concluido

--- envia_email/views.py
+++ envia_email/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 def envia_email(request):
-    send_mail('Troca de senha', 'link','naorespondacryptofinance@outlook.com',['aurelio74123@gmail.com'])
-    return HttpResponse('PAGINA DE EMAIL')
+    #send_mail('Troca de senha', 'link','naorespondacryptofinance@outlook.com',['aurelio74123@gmail.com'])
+    return HttpResponse('PLATAFOMRA')
