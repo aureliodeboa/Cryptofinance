2409e338cf82bedc03631d24cb6d72dd45ec1b10
Aurelio <aurelio74123@gmail.com>
2023-02-19 17:57:40 -0300

    apipronto

--- plataforma/views.py
+++ plataforma/views.py
@@ -5,9 +5,9 @@ import requests
 
 @login_required
 def plataforma(request):
-    api='https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=market_cap_desc&per_page=200&page=1&sparkline=false'
-    dados= requests.get(api).json()
-    return render(request, 'plataforma/plataforma.html',context={'datas':dados})
+    api='https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=market_cap_desc&per_page=201&page=1&sparkline=false'
+    raw_data= requests.get(api).json()
+    return render(request, 'plataforma/plataforma.html',context={'datas':raw_data})
 
 
  
