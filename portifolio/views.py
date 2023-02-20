5c677ffaed1c308f4de8dc59d644c2e9c8d5deac
Aurelio <aurelio74123@gmail.com>
2023-02-20 10:47:31 -0300

    teminado carteira

--- portifolio/views.py
+++ portifolio/views.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from portifolio.models import Carteira
+from django.contrib.auth.decorators import login_required
 
 import requests
-
+@login_required
 def carteira(request):
     if request.method=='POST':
         nome_moeda= request.POST.get('nome_moeda')
@@ -21,20 +22,26 @@ def carteira(request):
         carteira_objs= Carteira.objects.filter(usuario=request.user)
         nome_moeda_list=[]
         preco_moeda_list=[]
-
+        imagem_list=[]
+        saldototal=float(0)
         for obj in carteira_objs:
             coin_api= f'https://api.coingecko.com/api/v3/coins/{obj.moeda}' #o nome que ta escrito no models
             raw_data_single= requests.get(coin_api).json()
             #print(raw_data_single)
             float_quantidade= float(obj.quantidade)
             preco_atual= raw_data_single['market_data']['current_price']['brl']
+            imagem=raw_data_single['image']['large']
             preco_total= float_quantidade * preco_atual
+            imagem_list.append(imagem)
             nome_moeda_list.append(obj.moeda)
             preco_moeda_list.append(preco_total)
+            saldototal+=float(preco_total)
 
-        final_list= zip(nome_moeda_list, preco_moeda_list)
+        final_list= zip(nome_moeda_list, preco_moeda_list,imagem_list)
         data={
-            'dataList':final_list
+            'dataList':final_list,
+            'saldo':saldototal,
+            'imagem':imagem_list
         }
 
         return render(request, 'carteira/carteira.html',context=data)
