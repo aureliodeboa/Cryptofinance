7222980c59102c6d1c209b5273e2d359071aafc1
Aurelio <aurelio74123@gmail.com>
2023-02-25 00:15:30 -0300

    tratamento de erros

--- portifolio/views.py
+++ portifolio/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from portifolio.models import Carteira
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 import requests
+
+
 @login_required
 def carteira(request):
     if request.method=='POST':
@@ -13,10 +15,18 @@ def carteira(request):
         simbolo_moeda= request.POST.get('simbolo_moeda')
         quantidade_moeda= request.POST.get('quantidade_moeda')
         if nome_moeda:
-            Carteira.objects.create(usuario=request.user,moeda=nome_moeda,symbol=simbolo_moeda, quantidade=quantidade_moeda)
-            print('TUDO SALVO MEU LINDO!!')
-            return HttpResponseRedirect(reverse('carteira'))
-
+            coin_api= f'https://api.coingecko.com/api/v3/coins/{nome_moeda}' #verificar se o nome digitado esta certo
+            nome= requests.get(coin_api).json()
+            try:
+                if(nome['error']=='coin not found'):
+                    messages.warning(request,'Nome incorreto, tente novamente!!')
+                    return HttpResponseRedirect(reverse('carteira'))
+            except:
+                Carteira.objects.create(usuario=request.user,moeda=nome_moeda,symbol=simbolo_moeda, quantidade=quantidade_moeda)
+                print('TUDO SALVO MEU LINDO!!')
+                return HttpResponseRedirect(reverse('carteira'))
+                
+          
     else:
         #https://api.coingecko.com/api/v3/coins/NAME
         carteira_objs= Carteira.objects.filter(usuario=request.user)
@@ -24,6 +34,7 @@ def carteira(request):
         preco_moeda_list=[]
         imagem_list=[]
         id_list=[]
+        quantidade_list=[]
         saldototal=float(0)
         for obj in carteira_objs:
             coin_api= f'https://api.coingecko.com/api/v3/coins/{obj.moeda}' #o nome que ta escrito no models
@@ -38,8 +49,8 @@ def carteira(request):
             preco_moeda_list.append(preco_total)
             saldototal+=float(preco_total)
             id_list.append(obj.id)
-
-        final_list= zip(nome_moeda_list, preco_moeda_list,imagem_list,id_list)
+            quantidade_list.append(obj.quantidade)
+        final_list= zip(nome_moeda_list, preco_moeda_list,imagem_list,id_list,quantidade_list)
         data={
             'dataList':final_list,
             'saldo':saldototal,
